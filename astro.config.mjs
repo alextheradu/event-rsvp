774b3a1883d0504a6fac5217a6651f42d6af119f
Sahil Deshmukh <hello@sahil.ink>
2026-03-15 19:48:10 -0700

    Disable origin check

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from '@astrojs/node';
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+  security: { checkOrigin: false },
   vite: {
     plugins: [tailwindcss()],
     ssr: {
